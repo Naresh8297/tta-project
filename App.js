ea199843611514c37dad1220cd5901af59e0bc1a
Naresh8297 <131869492+Naresh8297@users.noreply.github.com>
2023-09-24 10:11:15 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TableLayout from './Components/Tablelayout'
+
+function App() {
+  return (
+    <div className="App">
+      <TableLayout />
+    </div>
+  );
+}
+
+export default App;
+
